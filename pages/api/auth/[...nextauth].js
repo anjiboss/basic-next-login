f788201e07e6d24c3113ced791f323f3261d15a3
Anji <anji.investhack@gmail.com>
2021-07-28 12:08:59 +0900

    set-up autho0

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,10 @@ import Providers from "next-auth/providers";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GitHub_ID,
-      clientSecret: process.env.GitHub_SECRET,
+    Providers.Auth0({
+      clientId: process.env.Auth0_CLIENT_ID,
+      clientSecret: process.env.Auth0_SECRET,
+      domain: process.env.Auth0_DOMAIN,
     }),
     // ...add more providers here
   ],
